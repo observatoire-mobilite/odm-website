9d3b065458971c582ae7c2245bfe84a6aa436db4
Gil Georges <ggeorges@doheem>
2023-04-20 03:52:22 +0200

    Add a search function to `BusMap` Fixes #35

--- src/LineMap/Search.js
+++ src/LineMap/Search.js
@@ -1,38 +1,27 @@
-import {useEffect, useState} from 'react';
+import {useMemo} from 'react';
 import Autocomplete from '@mui/material/Autocomplete';
 import TextField from '@mui/material/TextField';
 import { useTheme } from '@mui/material/styles';
+import { useLineMapStore } from './store';
 
 
 export default function Search({}) {
-    const [places, setPlaces] = useState(null)
     const theme = useTheme()
+    const [zoomTo, lineMap] = useLineMapStore((state) => [state.zoomTo, state.lineMap])
     
-    useEffect(() => {
-        setPlaces(null)
-        fetch('data/publictransport/busmap.json')
-        .then((r) => r.json())
-        .then((dta) => {
-            const places = dta.stops.map((stop) => ({id: stop.id, label: stop.label})).sort((a, b) => ('' + a.label).localeCompare(b.label))
-            const lines = dta.lines.map((line) => ({id: line.id, label: line.line})).sort((a, b) => ('' + a.label).localeCompare(b.label))
-            setPlaces([...places, ...lines])
-        }).catch((e) => {
-            console.log(e.message)
-        });
-    }, [])
-
-    /*const places = useMemo(() => {
-        const places = stops.map((stop) => ({id: stop.id, label: stop.label})).sort((a, b) => ('' + a.label).localeCompare(b.label))
-        const lines = lines.map((line) => ({id: line.id, label: line.line})).sort((a, b) => ('' + a.label).localeCompare(b.label))
+    const places = useMemo(() => {
+        if (lineMap == null) return null
+        const places = lineMap.stops.map((stop) => ({id: stop.id, label: stop.label, zoom: {x: stop.cx, y: stop.cy, z: 3}})).sort((a, b) => ('' + a.label).localeCompare(b.label))
+        const lines = lineMap.lines.map((line) => ({id: line.id, label: line.line})).sort((a, b) => ('' + a.label).localeCompare(b.label))
         return [...places, ...lines]
-    }, [stops, lines])*/
+    }, [lineMap])
 
     /* BTW: I found the value={null} bit in one of the lowest voted answers on
        https://stackoverflow.com/questions/59790956/material-ui-autocomplete-clear-value */
     return (
         <Autocomplete
             loading={places === null}
-            style={{position: 'absolute', top: '5rem', right: '3rem'}}
+            style={{position: 'absolute', top: '1rem', right: '4rem'}}
             autoHighlight
             openOnFocus
             id="map-search"
@@ -40,11 +29,11 @@ export default function Search({}) {
             getOptionLabel={(option) => `${option.label}`}
             groupBy={(option) => option?.label?.charAt(0)}
             sx={{ width: 300 }}
-            renderInput={(params) => <TextField {...params} style={{backgroundColor: 'white'}} label="Search" />}
+            renderInput={(params) => <TextField {...params} style={{backgroundColor: 'white'}} label="Search for places or bus-lines" />}
             blurOnSelect
             clearOnBlur
             value={null}
-            onChange={(evt, place) => {alert('image I just zoomed')}}
+            onChange={(evt, place) => {zoomTo(place.zoom)}}
         />
     )
 }
