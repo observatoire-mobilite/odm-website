940549dd30874b42df663640b63d226a471f653e
Gil Georges <ggeorges@doheem>
2023-05-02 00:41:57 +0200

    Fix: RoadTraffic: larger map initially

--- src/RoadTraffic/RoadTraffic.js
+++ src/RoadTraffic/RoadTraffic.js
@@ -34,7 +34,7 @@ function Layout({comment, children}) {
       <Grid item xs={12} sx={{p: 2}}>
         <Typography variant="caption">Comptages automatiques de l'Administration des ponts et chaussées - infos détaillées sur <a href="https://pch.gouvernement.lu/fr/administration/competences/comptage-trafic.html" target="_blank">Comptage du trafic</a> - données disponibles en <a href="https://data.public.lu/fr/datasets/pch-comptage-du-trafic-sur-le-reseau-routier-national/" target="_blank">OpenData</a></Typography>
       </Grid>
-      <Grid item xs={12} md={6} lg={5} minHeight="40vh">
+      <Grid item xs={12} md={6} lg={5} sx={{height: {xs: '40vh', md: '80vh'}}}>
         <StationMap />
       </Grid>
       <Grid item xs={12} md={6} lg={7}>
